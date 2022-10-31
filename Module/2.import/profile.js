d519b648cb68b311e286df57ddd8d64a2caecdcf
keshanwen <3640@winnermedical.com>
2022-10-31 11:38:10 +0800

    feat: import初探

---
+++ Module/2.import/profile.js
@@ -0,0 +1,26 @@
+export let a = 'aaaa'
+
+let b = 'bbb'
+let c = 'cccc'
+
+export {
+    b,
+    c
+}
+
+export function fn1() {}
+
+let fn2 = function() {}
+
+export {
+    fn2
+}
+
+
+let obj = {
+    name: 'kebi'
+}
+
+export {
+    obj
+}
