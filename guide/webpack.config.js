8fbd4b486c891ceb540eda94ef185a0f2e7777ba
keshanwen <3640@winnermedical.com>
2022-11-04 11:46:58 +0800

    feat:通过指令加入环境变量参数

--- guide/webpack.config.js
+++ guide/webpack.config.js
@@ -1,37 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { env } = require('process');
 
-module.exports = {
-  mode: 'none', 
+module.exports = env => {
 
-  devtool: 'inline-source-map',
+    console.log('NODE_ENV: ', env) // 'local'
+    console.log('Production: ', env) // true
 
-  entry: './src/index.ts',
+  return {
+    mode: 'none', 
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Output Management'
-    }),
-    new CleanWebpackPlugin()
-  ],
-
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    devtool: 'inline-source-map',
+  
+    entry: './src/index.ts',
+  
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+  
+    plugins: [
+      new HtmlWebpackPlugin({
+          title: 'Output Management'
+      }),
+      new CleanWebpackPlugin(),
+    ],
+  
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+  
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
   }
 };
