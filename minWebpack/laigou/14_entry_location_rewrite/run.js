250d149b08f4a998c3cf75a72e2d6ab6368f605c
keshanwen <3640@winnermedical.com>
2022-12-06 14:21:38 +0800

    feat；webpack 起点初探

---
+++ minWebpack/laigou/14_entry_location_rewrite/run.js
@@ -0,0 +1,11 @@
+let webpack = require('webpack')
+let options = require('./webpack.config.js')
+
+let complier = webpack(options)
+
+
+complier.run(function(err, stats) {
+    console.log('run~~~~~~~~~~~~~~~~~~')
+    // console.log(err)
+    // console.log(stats.toJson())
+})
