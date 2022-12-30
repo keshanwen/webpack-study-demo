4129cc90a365177db03a7d9ee02fe9801eee2a00
keshanwen <3640@winnermedical.com>
2022-12-30 16:33:52 +0800

    feat: 增加打包进度条插件

--- webpackOnselfConfig/build/webpack.common.js
+++ webpackOnselfConfig/build/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackBar = require('webpackbar')
 
 module.exports = {
   entry: {
@@ -9,6 +10,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Production',
     }),
+    new WebpackBar()
   ],
   output: {
     filename: '[name].bundle.js',
