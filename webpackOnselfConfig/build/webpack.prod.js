b978955cb662784006eb473999b290c6b73cdd36
keshanwen <3640@winnermedical.com>
2023-01-05 15:24:20 +0800

    feat:代码分割

--- webpackOnselfConfig/build/webpack.prod.js
+++ webpackOnselfConfig/build/webpack.prod.js
@@ -20,5 +20,31 @@ module.exports = merge(baseConfig, {
       analyzerMode: "disabled",
       generateStatsFile: true,
     }),
-  ]
+  ],
+
+  optimization: {
+    splitChunks: {
+      // 选择对哪些文件进行拆分，默认是async，即只对动态导入的文件进行拆分
+      chunks: "all",
+      // 提取chunk的最小体积
+      minSize: 20000,
+      // 要提取的chunk最少被引用次数
+      minChunks: 1,
+      // 对要提取的trunk进行分组
+      cacheGroups: {
+        // 匹配node_modules中的三方库，将其打包成一个trunk
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          priority: -10,
+        },
+        default: {
+          // 将至少被两个trunk引入的模块提取出来打包成单独trunk
+          minChunks: 2,
+          name: "default",
+          priority: -20,
+        },
+      },
+    },
+  },
 });
