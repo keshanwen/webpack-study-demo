1a69902050d2f5b1e3e9e5e9207ea0aaba68868a
keshanwen <3640@winnermedical.com>
2023-12-29 14:51:46 +0800

    feat: min-webpack 执行 Compiler 对象的 run 方法

--- min-webpack/debugger.js
+++ min-webpack/debugger.js
@@ -4,14 +4,14 @@ const webpackOptions = require("./webpack.config.js");
 const compiler = webpack(webpackOptions);
 
 
-console.log(compiler)
-// compiler.run((err, stats) => {
-//   console.log(err);
-//   console.log(
-//     stats.toJson({
-//       assets: true, //打印本次编译产出的资源
-//       chunks: true, //打印本次编译产出的代码块
-//       modules: true, //打印本次编译产出的模块
-//     })
-//   );
-// });
+compiler.run((err, stats) => {
+  // console.log(err);
+  // console.log(
+  //   stats.toJson({
+  //     assets: true, //打印本次编译产出的资源
+  //     chunks: true, //打印本次编译产出的代码块
+  //     modules: true, //打印本次编译产出的模块
+  //   })
+  // );
+  console.log('run ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+});
--- min-webpack/webpack.js
+++ min-webpack/webpack.js
@@ -36,7 +36,22 @@ class Compiler {
 
   }
 
-  run(callback) { }
+  complie(callback) {
+    /*
+      虽然 webpack 只有一个 Complier， 但是它每次编译会产生一个新的 Compilation，这里主要是为了考虑到watch 模式， 它会在启动时先编译一次， 然后监听文件变化， 如果发生变化会重新开始编译。每次编译会产生一个新的Compliation 代表每次的编译结果
+    */
+    let compilation = new Compilation(this.options)
+    compilation.build(callback) // 执行compilation的build 方法进行编译， 编译成功之后执行回调
+  }
+
+  // 第四步： 执行 Complier 对象的run方法开始执行编译
+  run(callback) {
+    this.hooks.run.call() // 在编译前触发 run 钩子执行，表示开始启动编译了
+    const onCompiled = () => {
+      this.hooks.done.call() // 当编译成功后触发 done 这个钩子执行
+    }
+    this.complie(onCompiled) // 开始编译，成功之后调用 onCompiled
+  }
 }
 
 
@@ -51,6 +66,21 @@ function webpack(webpackOptions) {
   }
 
   return complier
+}
+
+class Compilation {
+  constructor(webpackOptions) {
+    this.options = webpackOptions
+    this.modules = {} // 本次编译所有生成出来的模块
+    this.chunks = [] // 本次编译产出的所有代码块，入口模块和依赖的模块打包在一起为代码块
+    this.assets = {} // 本次编译产出的资源文件
+    this.fileDependencies = [] // 本次打包涉及到的文件， 这里主要是为了实现 watch 模式下监听文件的变化， 文件发生变化后会重新进行编译
+  }
+
+  build(callback) {
+    // 这里开始做编译工作， 编译成功执行callback
+    callback()
+  }
 }
 
 module.exports = {
