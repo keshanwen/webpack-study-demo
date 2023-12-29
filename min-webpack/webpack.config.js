b70928c7e7bf634ffb3a278f2c5e18b4c4eddcd5
keshanwen <3640@winnermedical.com>
2023-12-29 13:53:09 +0800

    feat: min-wepack 初始化

---
+++ min-webpack/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development", //防止代码被压缩
+  entry: "./src/index.js", //入口文件
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  devtool: "source-map", //防止干扰源文件
+  //第七步加
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+  },
+};
