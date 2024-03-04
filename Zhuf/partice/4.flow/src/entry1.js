325dab8228d729640dfcbc888d0a29d799b9c1a4
keshanwen <3640@winnermedical.com>
2024-03-04 16:53:30 +0800

    feat: 去掉多余文件

--- Zhuf/partice/4.flow/src/entry1.js
+++
@@ -1,6 +0,0 @@
-let title = require('./title');
-let title2 = require('./title2')
-
-console.log(title, 'entry1');
-console.log(title2, 'entry1 title2')
-console.log('hello wrold')
--- Zhuf/partice/4.flow/src/title.js
+++
@@ -1,5 +0,0 @@
-const a = require('./a')
-
-console.log(a, 'title a')
-
-module.exports = 'title';
