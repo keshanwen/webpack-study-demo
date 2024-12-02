243b81d7c5a6d254a4825d807781b9aad471b939
keshanwen <3640@winnermedical.com>
2024-12-02 11:02:38 +0800

    feat: 配置文件初始化

---
+++ rewrite/simpleWebpack/cli.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+const webpackOptions = require('./webpack.config.js')
+
+const compiler = webpack(webpackOptions)
+
+compiler.run( (err, stats) => {
+    // console.log(err, 'err')
+    // console.log(
+    //     stats.toJson({
+    //         entries: true,
+    //         chunks: true,
+    //         modules: true,
+    //         assets: true
+    //     })
+    // )
+})
