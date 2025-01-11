2aba9d7b77decf268143abce36956633bf7bcb35
keshanwen <624953505@qq.com>
2025-01-11 10:36:52 +0800

    feat: sourceMap 源码映射

--- zfRewrite/src/index.js
+++ zfRewrite/src/index.js
@@ -1,6 +1,2 @@
-// import png from "./assets/logo.png";
-// import ico from "./assets/logo.ico";
-// import jpg from "./assets/logo.jpg";
-import txt from "./assets/logo.txt";
-// console.log(png, ico, jpg, txt);
-console.log(txt);
+let title = require("./title.js");
+console.log(title);
--- zfRewrite/webpack.config.js
+++ zfRewrite/webpack.config.js
@@ -1,78 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-/*  
-
-    --mode用来设置模块内的process.env.NODE_ENV
-        会将 DefinePlugin 中 process.env.NODE_ENV 的值设置为 mode 对应的值   
-        可以在任意模块内通过 process.env.NODE_ENV 获取当前的环境变量
-        但无法在node环境(webpack 配置文件中)下获取当前的环境变量
-
-    cross-env用来设置node环境的process.env.NODE_ENV
-
-    DefinePlugin用来设置模块内的全局变量
-*/
-
-console.log("NODE_ENV", process.env.NODE_ENV); // 在配置文件中获取不到环境变量
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-  // mode: "development",
-
+  mode: "development",
+  devtool: "source-map",
+  // devtool: "none",
   entry: "./src/index.js",
-
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    // publicPath: "https://www.baidu.com/", //表示的是打包生成的index.html文件里面引用资源的前缀
   },
-
-  module: {
-    rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      {
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
-      },
-      {
-        test: /\.png$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.ico$/,
-        type: "asset/inline",
-      },
-      {
-        test: /\.txt$/,
-        type: "asset/source",
-      },
-      {
-        test: /\.jpg$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 4 * 1024, // 4kb
-          },
-        },
-      },
-    ],
-  },
-
+  module: {},
   plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    // new webpack.DefinePlugin({
-    //   "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    // }),
+    new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ["**/*"] }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
   ],
-
-  devServer: {
-    static: path.resolve(__dirname, "public"),
-    port: 8080,
-    open: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        pathRewrite: { "^/api": "" },
-      },
-    },
-  },
+  devServer: {},
 };
