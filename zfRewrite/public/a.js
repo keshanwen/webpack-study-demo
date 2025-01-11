9861150a27a95b631896173802a1dd6ab7d1d9d6
keshanwen <624953505@qq.com>
2025-01-11 16:43:19 +0800

    pref: resolve  extenstion

---
+++ zfRewrite/public/a.js
@@ -0,0 +1,1 @@
+"heelo a.js";
---
+++ zfRewrite/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    // publicPath: "https://www.baidu.com/", //表示的是打包生成的index.html文件里面引用资源的前缀
+  },
+
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.png$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.ico$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.txt$/,
+        type: "asset/source",
+      },
+      {
+        test: /\.jpg$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024, // 4kb
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".css"],
+  },
+
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    port: 8080,
+    open: true,
+  },
+};
